e8f01dcca33bd04d93cdb0504710d64bb4676f71
MVJaburov <aburovmatvej@gmail.com>
2024-08-07 23:20:15 +0500

    check console

--- klippy/extras/knob.py
+++ klippy/extras/knob.py
@@ -5,7 +5,7 @@
 
 import logging
 import socket
-
+import threading
 REPORT_TIME = 1.0
 
 
@@ -17,9 +17,10 @@ class KnobSlave:
         self.gcode_move = self.printer.lookup_object("gcode_move")
         self.gcode = self.printer.lookup_object("gcode")
         self.server = self.printer.lookup_object("webhooks")
-        self.sample_timer = self.reactor.register_timer(self.update)
+        self.sample_timer = self.reactor.register_timer(self.poll_serial)
         self.server.register_endpoint("knob/change_param", self.change_param)
         self.printer.register_event_handler('klippy:connect', self.handle_connect)
+        self.not_working = True
 
     def handle_connect(self):
         self.reactor.update_timer(self.sample_timer, self.reactor.NOW)
@@ -32,9 +33,18 @@ class KnobSlave:
     def get_status(self, eventtime):
         return {'extruder_speed': self.gcode_move.extrude_factor * 100}
     
-    def update(self, eventtime):
-        self.i += 1 
+    def update(self):
+        self.not_working = False
+        self.i += 1
         logging.info(f'CUR: {self.i}')
+        print(f'CUR: {self.i}')
+        self.not_working = True
+    
+    def poll_serial(self, eventtime):
+        if self.not_working:
+            thread = threading.Thread(target=self.update)
+            thread.start()
+
         measured_time = self.reactor.monotonic()
         return measured_time + REPORT_TIME
 
